0ab84dca59532c5370aad58a548af33e39ba77fb
Kivanc Cakmak <kivanc.cakmak@airties.com>
2015-12-30 18:36:10 +0200

    feat: turkey regulation considered

--- plate_reader.py
+++ plate_reader.py
@@ -1,4 +1,5 @@
 import subprocess
+from car_recorder import CarRecorder
 
 ALPR_INDEX = {}
 ALPR_INDEX['plate'] = 1
@@ -6,6 +7,63 @@ ALPR_INDEX['confidence'] = -1
 
 FILE_PATH = '/home/kivi/Downloads/plaka.jpg'
 CMD = 'alpr -c eu {plate_img}'
+
+def tr_plate_check(plate):
+    """Turkey's plate number in format of.
+    int(2 char) string(varies) int(varies)
+    :plate: string
+    :returns: bool and string
+        bool -> whether plate is valid for Turkey
+        string -> fixed plate number
+    """
+    plate = list(plate)
+    if str(plate[0]) == 'O':
+        plate[0] = '0'
+    if str(plate[1]) == 'O':
+        plate[1] = '0'
+    try:
+        val = int(plate[0])
+    except ValueError:
+        return False, ''
+    try:
+        val = int(plate[1])
+    except ValueError:
+        return False, ''
+
+    i = 2
+    str_end = -1
+    while i < len(plate) and str_end == -1:
+        print "i: {}".format(i)
+        print "plate[i]: {}".format(str(plate[i]))
+        try:
+            val = int(str(plate[i]))
+            print "val: {}".format(val)
+            str_end = i
+        except ValueError:
+            print "value error"
+        i += 1
+
+    print "str_end: {}".format(str_end)
+    for i in range(str_end, len(plate)):
+        print "i: {}".format(i)
+        if plate[i] == 'O':
+            plate[i] = '0'
+        else:
+            try:
+                val = int(plate[i])
+            except ValueError:
+                return False, ''
+    return True, ''.join(plate)
+
+def db_check(plate):
+    """ checks database to seek plate.
+    :plate: string
+        plate number
+    """
+    records = CarRecorder(None, None, None, 
+            None, None, None, 'data/garage.sqlite')
+    rows = records.get_plate_records(plate, 'car_info')
+    return rows
 
 class PlateRead(object):
 
@@ -16,6 +74,7 @@ class PlateRead(object):
     def __init__(self, file_path):
         """Initializer function """
         self.file_path = file_path
+        self.result = []
 
     def openalpr_run(self):
         """runs openalpr and returns results in array.
@@ -23,7 +82,6 @@ class PlateRead(object):
         :returns: array
             [[plate, confidence], [], ...]
         """
-        result = []
         plate = ''
         confidence = ''
         alpr_cmd = CMD.format(plate_img=self.file_path)
@@ -35,8 +93,34 @@ class PlateRead(object):
             if idx != 0 and idx != len(out_lines) - 1:
                 plate = line.split()[ALPR_INDEX['plate']]
                 confidence = line.split()[ALPR_INDEX['confidence']]
-                result.append({'conf': confidence, 'plate': plate})
-        return result
+                self.result.append(
+                        {'conf': confidence, 'plate': plate}
+                        )
+        return self.result
+
+    def plate_check(self):
+        result = self.openalpr_run()
+        print result
+        result_info = {}
+        if len(result) == 0:
+            result_info['conf'] = '0'
+            result_info['plate'] = 'NoN'
+            return False, result_info
+
+        result_info = {}
+        for val in result:
+            state, plate = tr_plate_check(str(val['plate']))
+            if state:
+                rows = db_check(plate)
+            else:
+                rows = []
+            print "check_plate: {}".format(str(val['plate']))
+            print "len(rows): {}".format(len(rows))
+            if len(rows) != 0:
+                result_info = rows
+                return True, result_info
+        result_info = result[0]
+        return False, result_info
 
 def main():
     """runs open alpr with system call,
