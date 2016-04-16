40ffa0935a07bbe2ef8df93e524381041e7b8167
Kivanc Cakmak <kivanc.cakmak@airties.com>
2016-04-16 23:10:56 +0300

    fix: wget added to initializer

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
- wget -O http://deb.openalpr.com/openalpr.gpg.key | sudo apt-key add -
+sudo apt-get update wget 
+wget -O http://deb.openalpr.com/openalpr.gpg.key | sudo apt-key add -
  echo "deb http://deb.openalpr.com/master/ openalpr main" | sudo tee /etc/apt/sources.list.d/openalpr.list
  sudo apt-get update
  sudo apt-get install openalpr openalpr-daemon openalpr-utils libopenalpr-dev
